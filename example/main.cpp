41f0282dae723fe1d2d3ef9b4793015560652e97
Pavel D. <138060212+drxvmrz@users.noreply.github.com>
2025-05-27 12:54:44 +0300

    Add files via upload
    Improvement

--- example/main.cpp
+++ example/main.cpp
@@ -17,27 +17,27 @@
 int main()
 {
     // Create the progress bar
-    // Maximum progress = 100.0 (default, optional)
-    // Intial progress (value where progress-bar starts) = 0.0 (default, optional)
-    // Lenght in terminal symbols = 50 (default, optional)
-    // !!! Progress is not equal to percents of progress, it's an absolute value !!!
-    ProgressBar bar{100.0, 0.0, 50};
+    // Maximum progress = 100.0
+    // Intial progress (value where progress-bar starts) = 0.0
+    // Length in terminal symbols = 50
+    
+    Progress::ProgressBar bar{100.0, 0.0};
 
-    // Simulation of some processing
-    for (int i = 0; i < bar.maxProgress; i++)
+    // Simulation of some process
+    for (int i = 0; i < round(bar.getMaxProgress()); i++)
     {
         if (i % 2 == 0)
         {
             // Printing some message in terminal when i is even number
-            bar.printWithProgress(1.0, "Progress added! Now it = %.3f", bar.progress);
+            bar(1.0) << "Now progress is " << bar.getCurrentProgress() << " percent" << std::endl; 
         }
         else
         {
             // When i is odd it only increase progress without any messages
-            bar.increaseProgress(1.0);
+            bar(1.0);
         }
         // Timer for simulation of any long process
-        SLEEP(10);
+        SLEEP(400);
     }
 
     return 0;
